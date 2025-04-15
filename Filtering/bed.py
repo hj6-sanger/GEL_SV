69e029218e70a625794f714482f199df633d99f7
hj6-sanger <94840292+hj6-sanger@users.noreply.github.com>
2025-04-15 15:55:52 +0100

    Create bed.py

---
+++ Filtering/bed.py
@@ -0,0 +1,20 @@
+import sys
+
+f=open(sys.argv[1])
+g=open('2_' + sys.argv[1],'w')
+
+fline = f.readline()
+while fline :
+	fline = fline.strip()
+	if not fline.startswith('#') :
+		fso = fline.split('\t')
+		lens1= float(fso[2]) - float(fso[1])
+		lens2= float(fso[5]) - float(fso[4])
+
+		if lens1 <= 50 or lens2 <=50 :
+			g.write(fline + '\n')
+	fline = f.readline()
+f.close()
+g.close()
+
+
