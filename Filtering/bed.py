bd2a623ab1fc36312a33741327fd42d4bde69db5
hj6-sanger <94840292+hj6-sanger@users.noreply.github.com>
2025-04-16 10:25:28 +0100

    Update bed.py

--- Filtering/bed.py
+++ Filtering/bed.py
@@ -2,6 +2,7 @@ import sys
 
 '''
 This script was called by "filtering.py".
+This script filters out SVs with a length < 50
 This script filters out SVs with imprecise breakpoints and rescues such SVs flagged as 'ColocalizedCanva'.
 '''
 
@@ -13,14 +14,14 @@ while fline :
 	fline = fline.strip()
 	if not fline.startswith('#') :
 		fso = fline.split('\t')
-		lens1= float(fso[2]) - float(fso[1])
-		lens2= float(fso[5]) - float(fso[4])
+		lens1= float(fso[4]) - float(fso[2])
 
-		if 'IMPRECISE' not in fline :
-			g.write(fline + '\n')
-		else :
-			if 'ColocalizedCanvas' in fline :
-				g.write(fline + '\n')
+		if lens1 >= 50 :
+			if 'IMPRECISE' not in fline :
+					g.write(fline + '\n')
+			else :
+				if 'ColocalizedCanvas' in fline :
+					g.write(fline + '\n')
 	fline = f.readline()
 f.close()
 g.close()
